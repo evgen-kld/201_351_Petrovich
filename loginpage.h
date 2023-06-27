cda103ff708b14b2eff8b2d29831f013ec17222a
evgen-kld <evgenkld@inbox.ru>
2023-06-27 11:20:39 +0200

    логика login page

--- loginpage.h
+++ loginpage.h
@@ -15,8 +15,18 @@ public:
     explicit LoginPage(QWidget *parent = nullptr);
     ~LoginPage();
 
+private slots:
+    void on_pushButton_clicked();
+
+private:
+    static const QString pinCode;
+
+private:
+    bool checkPinCode(const QString& enteredPin) const;
+
 private:
     Ui::LoginPage *ui;
 };
+// Поле для хранения пин-кода
 
 #endif // LOGINPAGE_H
