de4a8aab456583304cfdfc633d9b4d28d6fcdbb2
evgen-kld <evgenkld@inbox.ru>
2023-06-27 15:21:00 +0200

    task2

---
+++ sputnic/Sputnic.cpp
@@ -0,0 +1,57 @@
+﻿#include <iostream>
+#include <windows.h>
+
+void debug(int pid) {
+
+	DebugActiveProcess(pid);
+	int temp = 0;
+
+    DEBUG_EVENT DBEvent;
+
+    while (1)
+    {
+        WaitForDebugEvent(&DBEvent, INFINITE);
+
+        if (DBEvent.dwDebugEventCode == EXIT_PROCESS_DEBUG_EVENT) {
+            // Процесс игры завершился
+            std::cout << "Game process exited." << std::endl;
+
+            HANDLE hProcess = OpenProcess(PROCESS_TERMINATE, FALSE, pid);
+
+            // Завершаем процесс-спутник
+            TerminateProcess(hProcess, 0);
+
+            // Завершаем приложение
+            ExitProcess(0);
+        }
+
+        ContinueDebugEvent(DBEvent.dwProcessId, DBEvent.dwThreadId, DBG_CONTINUE);
+    }
+}
+
+int main()
+{
+
+    STARTUPINFO si;
+    PROCESS_INFORMATION pi;
+    ZeroMemory(&si, sizeof(si));
+    si.cb = sizeof(si);
+    ZeroMemory(&pi, sizeof(pi));
+    wchar_t cmdLine[] = L"C:/Users/User/Documents/build-ExamApplication-Desktop_Qt_6_5_0_MSVC2019_64bit-Debug/debug/ExamApplication.exe";
+
+    if (CreateProcess (
+        cmdLine, NULL,
+        NULL, NULL,
+        TRUE, NULL,
+        NULL, NULL,
+        &si, &pi)) {
+        std::cout << "Create Process succes!" << std::endl;
+        std::cout << "Create Process pid = " <<std::dec <<pi.dwProcessId <<std::endl;
+    }
+    else {
+        std::cout << "Create Process failed!" << std::endl;
+    }
+	setlocale(LC_ALL, "rus");
+        debug(pi.dwProcessId);
+}
+
